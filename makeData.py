4a9466e82d9f331d0e3febb4f22092ef543a54d9
Harvey Tang <harvey.xw.tang@gmail.com>
2016-04-03 20:54:50 -0400

    Formatting changes across the board, all search models return how many times the NN returns a success

--- makeData.py
+++ makeData.py
@@ -1,68 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from multiprocessing import Process,Pipe
-import shutil,os
+from multiprocessing import Process, Pipe
+import shutil
+import os
 import random
 
-def makeSquare(layers,length):
-    square = np.ones((layers, length, length), dtype=np.uint8)*255
-    square[0] = np.zeros((length, length), dtype=np.uint8)
+
+def makeSquare(layers, length, color=[0, 255, 255]):
+    square = np.ones((layers, length, length), dtype=np.uint8)
+    ind = range(layers) if layers > 1 else [0]
+    for layer in ind:
+        square[layer] = square[layer]*color[layer]
     return square
 
-def makeTriangle(layers, height):
+
+def makeTriangle(layers, height, color=[255, 255, 0]):
     triangle = np.ones((layers, height, height), dtype=np.uint8)*255
-    ind = 2 if layers>1 else 0
+    for layer_ind in range(len(color)):
+        triangle[layer_ind] = triangle[layer_ind]*color[layer_ind]
+    ind = range(layers) if layers > 1 else [0]
     for i in range(height):
-        triangle[ind, i, :i+1] = np.zeros(i+1, dtype=np.uint8)
+        for layer in ind:
+            triangle[layer, i, :i+1] = np.ones(i+1, dtype=np.uint8)*color[layer]
     return triangle
+
 
 def makePeriImages(triLoc, squaLoc, imgSize=400):
     stimSize = imgSize//10 - 1
-    tinyImage= np.ones((1, imgSize//5, imgSize//5), dtype=np.uint8)*255
+    tinyImage = np.ones((1, imgSize//5, imgSize//5), dtype=np.uint8)*255
 
-    #Make stimuli
-    tinySquare   = makeSquare(1, stimSize//5)
+    # Make stimuli
+    tinySquare = makeSquare(1, stimSize//5)
     tinyTriangle = makeTriangle(1, stimSize//5)
 
-    #Place in Images
-    tinyBorder = ((stimSize + 1)//2)//5 #Avoid clipping edge
-    for x,y in triLoc:
+    # Place in Images
+    tinyBorder = ((stimSize + 1)//2)//5  # Avoid clipping edge
+    for x, y in triLoc:
         tinyImage[0, y//5-tinyBorder+1:y//5+tinyBorder, x//5-tinyBorder+1:x//5+tinyBorder] = tinyTriangle
-    x,y = squaLoc
+    x, y = squaLoc
     tinyImage[0, y//5-tinyBorder+1:y//5+tinyBorder, x//5-tinyBorder+1:x//5+tinyBorder] = tinySquare
 
     loc = y//(imgSize//4) * 4 + x//(imgSize//4)
     return (tinyImage, loc)
 
+
 def makeFoveImages(triLoc, squaLoc, imgSize=400):
     stimSize = imgSize//10 - 1
-    bigImage = np.ones((3, imgSize,    imgSize   ), dtype=np.uint8)*255
+    bigImage = np.ones((3, imgSize, imgSize), dtype=np.uint8)*255
 
-    #Make stimuli
-    bigSquare   = makeSquare(3, stimSize)
+    # Make stimuli
+    bigSquare = makeSquare(3, stimSize)
     bigTriangle = makeTriangle(3, stimSize)
 
-    #Place in Images
-    bigBorder = (stimSize + 1)//2 #Avoid clipping edge
-    for x,y in triLoc:
+    # Place in Images
+    bigBorder = (stimSize + 1)//2  # Avoid clipping edge
+    for x, y in triLoc:
         bigImage[:, y-bigBorder+1:y+bigBorder, x-bigBorder+1:x+bigBorder] = bigTriangle
-    x,y = squaLoc
+    x, y = squaLoc
     bigImage[:, y-bigBorder+1:y+bigBorder, x-bigBorder+1:x+bigBorder] = bigSquare
 
     loc = y//(imgSize//4) * 4 + x//(imgSize//4)
     return (bigImage, loc)
 
-def parallelizedLoops(numImgs,numDistractors,conn,makeFove=True,makePeri=True):
-    np.random.seed(random.randint(0,4294967295))
-    triLocs  = np.random.randint(40, 360, (numImgs ,numDistractors, 2))
+
+def parallelizedLoops(numImgs, numDistractors, conn, makeFove=True, makePeri=True):
+    np.random.seed(random.randint(0, 4294967295))
+    triLocs = np.random.randint(40, 360, (numImgs, numDistractors, 2))
     squaLocs = np.random.randint(40, 360, (numImgs, 2))
-    
+
     results = {}
     if makeFove:
         results['fovealImages'] = np.zeros((numImgs, 3, 400, 400), dtype=np.uint8)
         results['fovealIndexes'] = np.zeros(numImgs, dtype=np.uint8)
         for i in range(numImgs):
-            fove = makeFoveImages(triLocs[i],squaLocs[i])
+            fove = makeFoveImages(triLocs[i], squaLocs[i])
             results['fovealImages'][i] = fove[0]
             results['fovealIndexes'][i] = fove[1]
 
@@ -77,16 +88,17 @@ def parallelizedLoops(numImgs,numDistractors,conn,makeFove=True,makePeri=True):
     conn.send(results)
     return
 
+
 def refactor(numImgs, numDistractors, makeFove, makePeri, iteration):
     numProcess = 4
     subNumImages = numImgs/numProcess
     parentConns, childConns = zip(*[Pipe() for _ in range(numProcess)])
-    procs = [Process(target=parallelizedLoops, 
-                     args=(subNumImages,numDistractors,childConns[_],makeFove,makePeri))
-                         for _ in range(numProcess)]
+    procs = [Process(target=parallelizedLoops,
+                     args=(subNumImages, numDistractors, childConns[_],
+                           makeFove, makePeri)) for _ in range(numProcess)]
     for proc in procs:
         proc.start()
-    
+
     data = {}
     if makeFove:
         data['fovealImages'] = []
@@ -95,24 +107,25 @@ def refactor(numImgs, numDistractors, makeFove, makePeri, iteration):
         data['peripheryImages'] = []
         data['peripheryIndexes'] = []
 
-    for proc,conn in zip(procs,parentConns):
+    for proc, conn in zip(procs, parentConns):
         d = conn.recv()
         for key in d:
             data[key].append(d[key])
         proc.join()
 
     for key in data:
-        np.save('data/tmp/%s%s.npy'%(key,iteration), np.concatenate(data[key]))
-    
+        np.save('data/tmp/%s%s.npy' % (key, iteration), np.concatenate(data[key]))
+
+
 def main(numImgs, numDistractors, makeFove=True, makePeri=True):
     if not os.path.exists('data/tmp'):
         os.makedirs('data/tmp')
-    
+
     subNumImages = 100
     numIter = numImgs/subNumImages
     for i in range(numIter):
-        refactor(subNumImages,numDistractors,makeFove,makePeri,i)
-        print '{} out of {}'.format(i,numIter)
+        refactor(subNumImages, numDistractors, makeFove, makePeri, i)
+        print '{} out of {}'.format(i, numIter)
 
     dataKinds = []
     if makeFove:
@@ -121,10 +134,10 @@ def main(numImgs, numDistractors, makeFove=True, makePeri=True):
     if makePeri:
         dataKinds.append('peripheryImages')
         dataKinds.append('peripheryIndexes')
-    
+
     for dataType in dataKinds:
         d = np.concatenate([np.load('data/tmp/'+dataType+str(i)+'.npy') for i in range(numIter)])
-        np.save('data/{}.npy'.format(dataType),d)
+        np.save('data/{}.npy'.format(dataType), d)
     shutil.rmtree('data/tmp/')
 
 if __name__ == '__main__':
--- searchModels.py
+++ searchModels.py
@@ -9,79 +9,105 @@ from helper import splitSectors
 # All the models return the number of tries it took them to get the right
 # answer, and the number of times they were sure they had the right answer.
 
-def fullRandom(foveImg, periImg, index):
+
+def fullRandom(foveImg, periImg, index, foveModel=None):
+    assert foveModel is not None, 'We need an initialized FoveaNet with loaded weights'
     tries = 0
+    foveSure = 0
     loc = -1
     saccades = []
-    while loc!=index:
-        tries+=1
-        loc = randint(0,16)
+    foveImSect = splitSectors(foveImg, objHalf=20)
+    while loc != index:
+        tries += 1
+        loc = randint(0, 16)
         saccades.append(loc)
-    return tries, tries, saccades
+        results = foveModel.predict(np.array([foveImSect[loc]]))
+        a = np.argmax(results)
+        if a == 0:
+            foveSure += 1
 
-def randomNoReplacement(foveImg, periImg, index):
+    return tries, foveSure, saccades
+
+
+def randomNoReplacement(foveImg, periImg, index, foveModel=None):
+    assert foveModel is not None, 'We need an initialized FoveaNet with loaded weights'
     tries = 0
-    guesses = sample(range(16),16)
+    foveSure = 0
+    foveImSect = splitSectors(foveImg, objHalf=20)
+    guesses = sample(range(16), 16)
     for guess in guesses:
-        tries+=1
-        if guess == index: 
+        tries += 1
+        a = np.argmax(foveModel.predict(np.array([foveImSect[guess]])))
+        if a == guess:
+            foveSure += 1
+        if guess == index:
             break
-    return tries, tries, guesses[:tries]
+    return tries, foveSure, guesses[:tries]
 
-def linearSearch(foveImg, periImg, index):
+
+def linearSearch(foveImg, periImg, index, foveModel=None):
+    assert foveModel is not None, 'We need an initialized FoveaNet with loaded weights'
     tries = 0
+    foveSure = 0
+    foveImSect = splitSectors(foveImg, objHalf=20)
     for sectInd in range(16):
-        tries+=1
+        tries += 1
+        a = np.argmax(foveModel.predict(np.array([foveImSect[sectInd]])))
+        if a == sectInd:
+            foveSure += 1
         if sectInd == index:
             break
-    return tries, tries, range(16)[:tries]
+    return tries, foveSure, range(16)[:tries]
+
 
 def periNetSearch(foveImg, periImg, index, periModel=None):
-    assert periModel!=None, 'We need an initialized PeripheryNet with loaded weights'
+    assert periModel is not None, 'We need an initialized PeripheryNet with loaded weights'
 
     periSectors = splitSectors(periImg, objHalf=4)
     predictions = periModel.predict(periSectors)
 
-    attention = np.argsort(predictions[:,0])[::-1]
+    attention = np.argsort(predictions[:, 0])[::-1]
     tries = 0
     for att in attention:
-        tries+=1
+        tries += 1
         if att == index:
             break
     return tries, tries, attention[:tries]
 
+
 def foveNetSearch(foveImg, periImg, index, foveModel=None):
-    assert foveModel!=None, 'We need an initialized FoveaNet with loaded weights'
+    assert foveModel is not None, 'We need an initialized FoveaNet with loaded weights'
 
     foveSectors = splitSectors(foveImg, objHalf=20)
     predictions = foveModel.predict(foveSectors)
 
-    attention = np.argsort(predictions[:,0])[::-1]
+    attention = np.argsort(predictions[:, 0])[::-1]
     tries = 0
     for att in attention:
-        tries+=1
+        tries += 1
         if att == index:
             break
     return tries, tries, attention[:tries]
 
+
 def neuralNetSearch(foveImg, periImg, index, periModel=None, foveModel=None):
-    assert periModel!=None, 'We need an initialized PeripheryNet with loaded weights'
-    assert foveModel!=None, 'We need an initialized FoveaNet with loaded weights'
-    
+    assert periModel is not None, 'We need an initialized PeripheryNet with loaded weights'
+    assert foveModel is not None, 'We need an initialized FoveaNet with loaded weights'
+
     periSectors = splitSectors(periImg, objHalf=4)
     foveSectors = splitSectors(foveImg, objHalf=20)
 
     predictions = periModel.predict(periSectors)
-    attention   = np.argsort(predictions[:,0])[::-1]
+    attention = np.argsort(predictions[:, 0])[::-1]
 
     foveSure = 0
     tries = 0
     for att in attention:
-        tries+=1
+        tries += 1
         a = np.argmax(foveModel.predict(foveSectors[att][np.newaxis,...]))
-        if att==index and a==0:
-            foveSure+=1
+        if att == index and a == 0:
+            foveSure += 1
             break
-        elif a==0:
-            foveSure+=1
-    return tries, foveSure, attention[:tries] #If tries gets to 16 then the model failed to find the right answer.
+        elif a == 0:
+            foveSure += 1
+    return tries, foveSure, attention[:tries]  # If tries gets to 16 then the model failed to find the right answer.
